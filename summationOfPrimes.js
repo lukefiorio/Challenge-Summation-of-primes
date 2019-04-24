52b993672036e8119aca8d12c9559322971d4a9e
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-24 09:35:27 -1000

    challenge completed

--- summationOfPrimes.js
+++ summationOfPrimes.js
@@ -1,3 +1,22 @@
 module.exports = function(limit) {
-  // do work here
-}
+  // determine whether any given number is prime
+  function isPrime(candidate) {
+    for (let i = 2; i <= Math.sqrt(candidate); i++) {
+      // if evenly divisible by any integer along the way => not prime
+      if (candidate % i === 0 || candidate === 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let primeSum = 0;
+  let candidates = 2;
+  while (candidates < limit) {
+    if (isPrime(candidates)) {
+      primeSum += candidates;
+    }
+    candidates++;
+  }
+  return primeSum;
+};
